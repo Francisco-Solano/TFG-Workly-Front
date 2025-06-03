92283ccea65a37fbd0c3040f2c2deae9ee27f2f3
Francisco-Solano <franciscosolanoleon@gmail.com>
2025-06-03 22:16:45 +0200

    FixConfig

--- FrontEnd/frontend/vite.config.ts
+++ FrontEnd/frontend/vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/TFG-Workly/',
+  base: '/TFG-Workly-Front/',
   css: {
     preprocessorOptions: {
       scss: {
